5ea4cb618b874b2f5e6dbfa67a5f39c073c584f2
taejinii <xowls177@gmail.com>
2023-07-20 01:21:24 +0900

    Faet: Button 컴포넌트 구현 #1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,23 @@
 import "../styles/globals.css";
-
+import Button from "@/components/ui/Button";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const test = () => {
+    alert("d");
+  };
   return (
     <html lang="ko">
       <body>
         <div className="fixed left-0 w-64 h-screen bg-blue-300">d</div>
         <main className="p-8 m-auto ml-64 ">
-          <div className="relative h-screen border-2">
-            <div className="w-[1028px] border-2 border-black h-screen">
-              피드
-            </div>
-            <div className="fixed flex flex-col bg-white border-2 border-red-500 top-8 right-8 w-[517px]">
-              <div>dddddddddddddddddddddddddddddddddddddddddddddddddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
-              <div>ddd</div>
+          <div className="flex h-screen gap-10 border-2 ">
+            <div className="h-screen border-2 border-black">피드</div>
+            <div className="flex-col bg-white border-2 border-red-500 top-8 right-8">
+              <div>ddddddddddddd</div>
+              <Button onClickAction={test}>Button</Button>
             </div>
           </div>
           {children}
---
+++ components/ui/Button/index.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { MouseEventHandler, ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode | string;
+  size?: "sm" | "md" | "lg" | "xl";
+  variant?: "primary" | "secondary";
+  disabled?: boolean;
+  className?: string;
+  fullWidth?: boolean;
+  onClickAction?: MouseEventHandler<HTMLButtonElement>;
+}
+const sizes = {
+  sm: "p-2 h-[36px] w-24",
+  md: "p-2 h-[42px] w-24",
+  lg: "p-2 h-[54px] w-24",
+  xl: "p-2 h-[66px] w-24",
+};
+const variants = {
+  primary: "bg-primary hover:bg-[#08995C] text-white",
+  secondary:
+    "bg-white border-2 border-primary  text-primary hover:border-[#08995C] hover:text-[#08995C]",
+};
+export default function Button({
+  children,
+  size = "md",
+  variant = "primary",
+  disabled = false,
+  className = "",
+  fullWidth = false,
+  onClickAction = () => console.error("onClick 이벤트가 정의되지 않았습니다."),
+}: ButtonProps) {
+  const sizeClass = sizes[size];
+  const variantClass = variants[variant];
+  const fullWidthClass = fullWidth && "w-full";
+  const disabledClass = disabled && "opacity-50 cursor-not-allowed";
+  return (
+    <button
+      className={`rounded-[10px] ${sizeClass} ${variantClass} ${fullWidthClass} ${disabledClass} ${className}`}
+      onClick={onClickAction}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
