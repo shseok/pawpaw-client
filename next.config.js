595a28d0ce2e00e70b89ede9787cb0dc736f18f0
JeonInguk <inguk12951@gmail.com>
2023-07-19 21:00:31 +0900

    Chore: 프로젝트 세팅

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = withBundleAnalyzer({
+    reactStrictMode: true,
+    images: {
+        formats: ["image/avif", "image/webp"],
+
+    },
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false, net: false, tls: false };
+
+        return config;
+    },
+});
+
+module.exports = nextConfig;
