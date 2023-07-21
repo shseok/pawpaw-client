b5e7dcfdb93768efd629bc0653792766eba275df
jiyeeeah <sije3757@snu.ac.kr>
2023-07-21 16:12:43 +0900

    Chore: 메인 페이지 폴더와 피드 파일 생성

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
         <div className="fixed left-0 w-64 h-screen bg-blue-300">d</div>
         <main className="p-8 m-auto ml-64 ">
           <div className="flex h-screen gap-10 border-2 ">
-            <div className="h-screen border-2 border-black">피드</div>
+            <div className="h-screen border-2 border-black"></div>
             <div className="flex-col bg-white border-2 border-red-500 top-8 right-8">
               <div>ddddddddddddd</div>
             </div>
