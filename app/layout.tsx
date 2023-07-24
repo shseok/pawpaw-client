d3a5f179884e94d61ac381bf57ed6d7ae63d7672
taejinii <xowls177@gmail.com>
2023-07-24 22:55:45 +0900

    Design: 채팅방 카드 레이아웃

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import ReactQueryProvider from "@/hooks/queries/ReactQueryProvider";
 import "styles/global.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ko">
       <ReactQueryProvider>
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import ChatCard from "@/components/ui/ChatCard";
+export default function Home() {
+  return (
+    <>
+      <ChatCard />
+    </>
+  );
+}
