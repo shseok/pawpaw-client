fc2bab0c47049a15ee78ad0a1bc93f166db5d5fb
JeonInguk <inguk12951@gmail.com>
2023-08-03 00:56:02 +0900

    Refactor: 폴더구조 재배치및 layout파일 수정 #15

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
-import "../styles/globals.css";
-import Sidebar from "@/components/ui/Sidebar/Sidebar";
+import "@/styles/globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>
-        <Sidebar />
-        <main className="p-8 m-auto ml-64">{children}</main>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
