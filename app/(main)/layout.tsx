bc588dc26f49f937d95156680ef147da257d6a4c
JeonInguk <inguk12951@gmail.com>
2023-08-03 01:06:19 +0900

    Chore: (main)-layout파일 html태그 삭제 #15

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,10 @@
 import Sidebar from "@/components/ui/Sidebar/Sidebar";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
-      <body>
-        <Sidebar />
-        <main className="p-8 m-auto ml-64">{children}</main>
-      </body>
-    </html>
+    <>
+      <Sidebar />
+      <main className="p-8 m-auto ml-64">{children}</main>
+    </>
   );
 }
