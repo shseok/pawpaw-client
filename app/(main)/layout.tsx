accee0dbbb548668d6153b8dd0e3c2c944f773ed
jiyeeeah <sije3757@snu.ac.kr>
2023-08-04 13:59:51 +0900

    Refactor: 놓친 코멘트 수정 #2

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,9 @@
-import Feed from "@/components/pages/Feed";
 import Sidebar from "@/components/ui/Sidebar/Sidebar";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Sidebar />
-      <Feed />
       <main className="p-8 m-auto ml-64">{children}</main>
     </>
   );
