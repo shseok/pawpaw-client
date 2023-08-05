ac3d62cc7eba21fecfb3b4f87a623dd3f8ccb341
jiyeeeah <sije3757@snu.ac.kr>
2023-08-05 12:36:53 +0900

    Design: 마진 변경 #2

--- components/ui/Feed/FeedHeader/Upload.tsx
+++ components/ui/Feed/FeedHeader/Upload.tsx
@@ -8,12 +8,8 @@ export default function Upload() {
       direction="column"
       className="bg-[#F5FFF6] p-[20px] border-[1px] rounded-[10px] w-full"
     >
-      <FlexBox className="w-full gap-[24px]">
-        <Avatar
-          size="xxl"
-          user_img="/Feed/desktop/tempUserProfilePic.svg"
-          user_name="수박이"
-        />
+      <FlexBox justify="between" className="w-full gap-[24px]">
+        <Avatar size="xxl" user_img="/Feed/desktop/tempUserProfilePic.svg" user_name="수박이" />
         <textarea
           className="bg-[#F5FFF6] w-full h-[77px] resize-none"
           placeholder="동네 주민들에게 즐거운 소식을 전해보세요!"
