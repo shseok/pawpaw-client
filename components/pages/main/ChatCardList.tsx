d352714a62d6af191da59f10a80034f9610fed8d
taejinii <xowls177@gmail.com>
2023-08-08 01:36:55 +0900

    Refactor: ChatCardTitle -> ChatCardHeader 로 리팩토링 및 ChatCardList 고정css 추가 #10

--- components/pages/main/ChatCardList.tsx
+++ components/pages/main/ChatCardList.tsx
@@ -8,9 +8,9 @@ export default function ChatCardList() {
   return (
     <FlexBox
       direction="column"
-      align="start"
+      justify="start"
       as="aside"
-      className="gap-3 max-w-[517px] invisible tablet:visible"
+      className="sticky top-0 hidden h-screen gap-5 p-8 bg-white tablet:flex"
     >
       <FlexBox justify="between" className="w-full">
         <h1 className="text-2xl font-bold">지금 뜨고있는 채팅방 🔥</h1>
--- components/ui/ChatCard/index.tsx
+++ components/ui/ChatCard/index.tsx
@@ -1,30 +1,23 @@
 "use client";
 import Button from "../Button";
 import FlexBox from "../FlexBox";
-import Tag from "../Tag";
-import ChatCardTitle from "./ChatCardTitle";
+import ChatCardHeader from "./ChatCardHeader";
 import ChatCardInfo from "./ChatCardInfo";
-import ShareIcon from "@/public/share.svg";
+import TagList from "../TagList";
+const tagList = ["자랑", "강아지", "고양이", "앵무새", "토끼토", "rhtm"];
 export default function ChatCard() {
   return (
     <FlexBox
       direction="column"
       align="start"
-      className="w-full max-h-[266px] p-8 rounded-[10px] gap-3 shadow-chatCard truncate"
+      className="max-h-[266px] w-full max-w-[517px]  p-8 rounded-[10px] gap-3 shadow-chatCard "
     >
-      <FlexBox justify="between" className="w-full ">
-        <ChatCardTitle title={"천하제일 내 반려동물 자랑방"} />
-        <button>
-          <ShareIcon />
-        </button>
-      </FlexBox>
-      <p>반려동물을 키우는 사람이라면 누구나 들어와서 자랑해주세요~</p>
-      <FlexBox className="gap-3">
-        {["20대이상", "자랑", "강아지", "고양이", "앵무새"].map((item) => {
-          return <Tag key={item} tagName={item} />;
-        })}
-        ...
-      </FlexBox>
+      <ChatCardHeader title={"천하제일 내 반려동천하제일 내 반려동물 자랑방"} />
+      <p className="w-full truncate">
+        반려동물을 키우는 사람이라면 누구나
+        들어와서자랑해자랑해주세요자랑해주세요주세요 자랑해주세요~
+      </p>
+      <TagList list={tagList} />
       <FlexBox justify="between" className="w-full">
         <ChatCardInfo
           participants={42}
@@ -36,4 +29,3 @@ export default function ChatCard() {
     </FlexBox>
   );
 }
-// 해당 컴포넌트는 클라이언트?서버? 서버컴포넌트면 입장하기를 Link 컴포넌트를 사용해야함
