e2e6f1293706067c346d0c9c3da9f85edf33aa31
taejinii <xowls177@gmail.com>
2023-08-08 15:12:01 +0900

    Design: Devider css 수정 #10

--- components/ui/Divider/index.tsx
+++ components/ui/Divider/index.tsx
@@ -6,6 +6,6 @@ export default function Devider({ type }: DeviderType) {
   if (type === "horizontal") {
     return <hr className="h-[1px] w-full bg-[#E9EBED]" />;
   } else {
-    return <hr className="h-full  w-[1px] bg-black" />;
+    return <hr className="h-full  w-[1px] bg-[#E9EBED]" />;
   }
 }
