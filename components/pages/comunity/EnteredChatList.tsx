16af727adab2ab1ff433569a2e9c14a9b5f08801
taejinii <xowls177@gmail.com>
2023-08-12 15:08:53 +0900

    :Design: 캐러셀 제목 폰트 수정 #20

--- components/pages/comunity/EnteredChatList.tsx
+++ components/pages/comunity/EnteredChatList.tsx
@@ -9,7 +9,7 @@ import Carousel from './Carousel';
 export default function EnteredChatList() {
   return (
     <FlexBox direction="column" className="gap-1">
-      <h1 className="flex w-full p-2">
+      <h1 className="flex w-full p-2 text-xl font-bold">
         <p className="text-green-600">지상최강감자</p>
         <p>님이 참여한 채팅방</p>
       </h1>
--- components/pages/comunity/RecommendChatList.tsx
+++ components/pages/comunity/RecommendChatList.tsx
@@ -8,7 +8,7 @@ import Carousel from './Carousel';
 export default function RecommendChatList() {
   return (
     <FlexBox direction="column" className="gap-1">
-      <h1 className="flex w-full p-2">
+      <h1 className="flex w-full p-2 text-xl font-bold">
         <p className="text-green-600">지상최강감자</p>
         <p>님에게 추천하는 신규 채팅방</p>
       </h1>
