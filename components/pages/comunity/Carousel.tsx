2e15f3932401831f753fd661ece6c672445173ee
taejinii <xowls177@gmail.com>
2023-08-12 16:26:13 +0900

    Fix: 캐러셀 옵션 수정 및 카드 헤더 css수정 #20

--- components/pages/comunity/Carousel.tsx
+++ components/pages/comunity/Carousel.tsx
@@ -7,14 +7,14 @@ export default function Carousel({ children }: { children: React.ReactNode }) {
   return (
     <Swiper
       className="w-full"
-      slidesPerView={1}
+      slidesPerView={1.1}
       spaceBetween={20}
       breakpoints={{
         640: {
-          slidesPerView: 2,
+          slidesPerView: 2.1,
         },
         1240: {
-          slidesPerView: 3,
+          slidesPerView: 3.1,
         },
       }}
     >
--- components/ui/ChatCard/ChatCardHeader.tsx
+++ components/ui/ChatCard/ChatCardHeader.tsx
@@ -4,8 +4,8 @@ import FlexBox from '../FlexBox';
 
 export default function ChatCardTitle({ title }: { title: string }) {
   return (
-    <FlexBox justify="between" className="w-full">
-      <FlexBox className="w-full gap-1 truncate">
+    <FlexBox justify="between" className="w-full gap-1">
+      <FlexBox className="gap-1 truncate ">
         <h3 className="w-full text-xl font-bold truncate">{title}</h3>
         <Badge />
       </FlexBox>
