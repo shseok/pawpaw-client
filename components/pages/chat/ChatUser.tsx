0ac17c971b7054631c6ac8c01078ace18de278ef
taejinii <xowls177@gmail.com>
2023-08-14 22:46:38 +0900

    Update: Avatar size 수정 #17

--- components/pages/chat/ChatUser.tsx
+++ components/pages/chat/ChatUser.tsx
@@ -5,7 +5,7 @@ import ArrowRightIcon from '@/public/arrow-right.svg';
 export default function ChatUser() {
   return (
     <FlexBox as="li" justify="start" className="w-full gap-3 px-5 py-3">
-      <Avatar user_img="/default.png" user_name="" size="large" />
+      <Avatar user_img="/default.png" user_name="" size="xl" />
       <FlexBox direction="column" align="start" className="gap-1 ">
         <FlexBox className="gap-1">
           <p className="font-bold">닉네임</p>
