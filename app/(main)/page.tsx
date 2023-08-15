bc69e04ae7eafa78b124f00db844ee44f9f982e2
jiyeeeah <sije3757@snu.ac.kr>
2023-08-15 23:23:20 +0900

    Design: 폰트 설정 다시 #13

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,6 +4,7 @@ import ChatCardList from '@/components/pages/main/ChatCardList';
 export default function Home() {
   return (
     <main className="flex w-full p-8 mt-[60px] tablet:mt-0">
+      <div className="header1">하이염</div>
       <Feed />
       <ChatCardList />
     </main>
