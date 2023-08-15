a2d6f844241b9fee62b9a11149473d1662cfb59e
taejinii <xowls177@gmail.com>
2023-08-15 23:11:15 +0900

    Feat: ChatRoom 분기 #17

--- app/(main)/chat/[roomId]/page.tsx
+++ app/(main)/chat/[roomId]/page.tsx
@@ -1,26 +1,18 @@
 import ChatUserList from '@/components/pages/chat/ChatUserList';
 import ChatRoomHeader from '@/components/pages/chat/ChatRoomHeader';
+import ChatRoom from '@/components/pages/chat/ChatRoom';
 
-export default function ChatRoom({ params }: { params: { roomId: string } }) {
-  const { roomId } = params;
+export default function ChatRoomPage({
+  params,
+}: {
+  params: { roomId: string };
+}) {
+  console.log(params);
   return (
     <main className="flex w-full">
-      <div className="w-full tablet:w-4/6 bg-[#F5FFF6] border-r-[1px]">
+      <div className="w-full h-full tablet:w-4/6 bg-[#F5FFF6] border-r-[1px]">
         <ChatRoomHeader title="천하제일 내 반려동물 자랑방" />
-        <div className="flex flex-col gap-5 p-10">
-          <div>
-            채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방
-            {roomId}
-          </div>
-          <div>
-            채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방
-            {roomId}
-          </div>
-          <div>
-            채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방채팅방
-            {roomId}
-          </div>
-        </div>
+        <ChatRoom />
       </div>
       <aside className="sticky top-0 hidden w-2/6 h-screen min-w-fit tablet:block">
         <ChatUserList />
---
+++ components/pages/chat/ChatRoom.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import FlexBox from '@/components/ui/FlexBox';
+
+export default function ChatRoom() {
+  return (
+    <FlexBox direction="column" className="gap-5 p-10">
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+      <div> ChatRoom ChatRoom ChatRoom ChatRoom ChatRoom</div>
+    </FlexBox>
+  );
+}
