e837963890d15d4b35054b7f4b334ed166eeb38c
taejinii <xowls177@gmail.com>
2023-08-27 02:55:19 +0900

    Feat: 스케줄 참여버튼 추가 및 디자인수정 #17

--- app/(main)/chat/[roomId]/page.tsx
+++ app/(main)/chat/[roomId]/page.tsx
@@ -12,7 +12,7 @@ export default function ChatRoomPage({
   return (
     <main className="flex w-screen">
       <ChatRoom roomId={params.roomId} />
-      <aside className="flex-col hidden w-2/6 h-screen min-w-fit tablet:flex">
+      <aside className="flex-col hidden w-3/6 h-screen min-w-fit tablet:flex">
         <ChatUserList />
         <Schedule />
       </aside>
--- components/pages/chat/Schedule.tsx
+++ components/pages/chat/Schedule.tsx
@@ -4,14 +4,14 @@ import ScheduleCard from './ScheduleCard';
 
 export default function Schedule() {
   return (
-    <FlexBox direction="column" className="gap-5 p-8 border h-1/2">
+    <FlexBox direction="column" className="gap-5 px-8 pt-8 h-1/2">
       <FlexBox as="header" justify="between" className="w-full">
         <h2 className="header2">스케줄</h2>
         <button type="button">
           <DotsIcon className="w-7 h-7" />
         </button>
       </FlexBox>
-      <ul className="flex flex-col gap-5 p-2 overflow-auto scrollbar-hide">
+      <ul className="flex flex-col w-full gap-5 p-2 overflow-auto scrollbar-hide">
         {Array.from({ length: 4 }).map((_, i) => (
           // eslint-disable-next-line react/no-array-index-key
           <ScheduleCard key={i} />
