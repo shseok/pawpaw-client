10bfc6490f6b36211e14f95b120bf74dfe49be34
jiyeeeah <sije3757@snu.ac.kr>
2023-09-04 14:49:48 +0900

    Feat:마이페이지 사이드 프로필 카드 구현 #26

---
+++ app/(main)/mypage/page.tsx
@@ -0,0 +1,14 @@
+import MyPageMain from '@/components/pages/mypage/MyPageMain';
+import SideProfile from '@/components/pages/mypage/SideProfile';
+import FlexBox from '@/components/ui/FlexBox';
+
+export default function CommunityPage() {
+  return (
+    <main className="w-full p-8">
+      <FlexBox justify="between" className="gap-8">
+        <SideProfile />
+        <MyPageMain />
+      </FlexBox>
+    </main>
+  );
+}
