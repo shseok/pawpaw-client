d526f681bbf738e4f9a57024c342685e263d357c
jiyeeeah <sije3757@snu.ac.kr>
2023-09-05 13:45:58 +0900

    Fix:ESLint disable

--- components/ui/Modal/Modal.tsx
+++ components/ui/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-useless-fragment */
 import { Dispatch, SetStateAction } from 'react';
 import { createPortal } from 'react-dom';
 import ModalWrapper from './ModalContentWrapper';
