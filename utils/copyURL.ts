98c7763c45dc788242a1f152a00425a35583104c
jiyeeeah <sije3757@snu.ac.kr>
2023-09-06 17:32:39 +0900

    Refactor: 쓸데없는 네이밍 변경

--- utils/copyURL.ts
+++ utils/copyURL.ts
@@ -1,4 +1,4 @@
-export default async function copyURLToClipboard() {
+export default async function copyURL() {
   try {
     await navigator.clipboard.writeText(window.location.href);
     alert('복사 성공');
