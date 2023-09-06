0b226e0df9f9e5c58496ee21fbacd2665ce1e7ae
jiyeeeah <sije3757@snu.ac.kr>
2023-09-06 17:30:56 +0900

    Refactor: copyURL 함수 컴포넌트화

--- components/ui/Feed/PostsList/PostDropdownButton.tsx
+++ components/ui/Feed/PostsList/PostDropdownButton.tsx
@@ -1,26 +1,7 @@
 /* eslint-disable react/no-array-index-key */
-import { Fragment } from 'react';
 import Dropdown from '@/components/ui/Dropdown/Dropdown';
 import DotsIcon from '@/public/tabler_dots.svg';
-
-function BookmarkOption() {
-  return <Dropdown.Item>북마크 추가</Dropdown.Item>;
-}
-function ShareOption() {
-  const copyToClipboard = async () => {
-    try {
-      await navigator.clipboard.writeText(window.location.href);
-      alert('복사 성공');
-    } catch {
-      alert('복사 실패');
-    }
-  };
-  return <Dropdown.Item event={copyToClipboard}>공유하기</Dropdown.Item>;
-}
-function ReportOption() {
-  return <Dropdown.Item>신고하기</Dropdown.Item>;
-}
-const OPTION_LIST = [BookmarkOption, ShareOption, ReportOption];
+import copyURLToClipboard from '@/utils/copyURL';
 
 export default function PostDropdownButton() {
   return (
@@ -29,9 +10,9 @@ export default function PostDropdownButton() {
         <DotsIcon className="w-6 h-6 tablet:w-7 tablet:h-7" />
       </Dropdown.Trigger>
       <Dropdown.Menu>
-        {OPTION_LIST.map((option, index) => (
-          <Fragment key={index}>{option()}</Fragment>
-        ))}
+        <Dropdown.Item>북마크 추가</Dropdown.Item>
+        <Dropdown.Item event={copyURLToClipboard}>공유하기</Dropdown.Item>
+        <Dropdown.Item>신고하기</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
