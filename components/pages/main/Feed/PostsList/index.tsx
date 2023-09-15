f2c1506b50662489ddfe6cab3400138ae8cced54
jiyeeeah <sije3757@snu.ac.kr>
2023-09-15 14:09:30 +0900

    Fix: 모달 열렸을때 스크롤 방지 (fix안됨) #2

--- components/pages/main/Feed/PostsList/index.tsx
+++ components/pages/main/Feed/PostsList/index.tsx
@@ -1,37 +1,29 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 
-'use client';
-
-import { useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import useCommentsQuery from '@/hooks/queries/PostCommentsQuery';
 import useGetInfiniteData from '@/hooks/queries/InfiniteData';
 import { Comment, Post } from '@/types/types';
 import FlexBox from '../../../../ui/FlexBox';
-import PostModal from './PostModal';
 import FeedPostCard from '../../../../ui/PostCard/FeedPostCard';
 
-export default function PostsList() {
+export default function PostsList({
+  setSelectedPost,
+  setSelectedComments,
+  setShowModal,
+}: {
+  setSelectedPost: Dispatch<SetStateAction<Post | null>>;
+  setSelectedComments: Dispatch<SetStateAction<Comment[] | undefined>>;
+  setShowModal: Dispatch<SetStateAction<boolean>>;
+}) {
   const { Observer, data: posts } = useGetInfiniteData({
     infiniteQueryKey: ['posts'],
   });
   const { data: comments } = useCommentsQuery();
 
-  // 모달창 구현에 필요
-  const [showModal, setShowModal] = useState(false);
-  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
-  const [selectedComments, setSelectedComments] = useState<
-    Comment[] | undefined
-  >(undefined);
-
   return (
-    <FlexBox direction="column" className="gap-[40px]">
-      <PostModal
-        showModal={showModal}
-        setShowModal={setShowModal}
-        post={selectedPost}
-        comments={selectedComments}
-      />
+    <FlexBox direction="column" className="gap-10">
       {posts?.pages?.map((post) => {
         // 댓글을 위한 부분인데 나중에 실제 api 연결하면 바뀔 예정
         const filteredComments = comments?.filter(
--- components/pages/main/Feed/index.tsx
+++ components/pages/main/Feed/index.tsx
@@ -1,12 +1,36 @@
+'use client';
+
+import { useState } from 'react';
 import PostsList from '@/components/pages/main/Feed/PostsList';
 import FlexBox from '@/components/ui/FlexBox';
 import FeedHeader from '@/components/pages/main/Feed/FeedHeader';
+import { Post, Comment } from '@/types/types';
+import PostModal from './PostsList/PostModal';
 
 export default function Feed() {
+  // 모달창 구현에 필요
+  const [showModal, setShowModal] = useState(false);
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
+  const [selectedComments, setSelectedComments] = useState<
+    Comment[] | undefined
+  >(undefined);
   return (
-    <FlexBox direction="column" className="gap-10">
+    <FlexBox
+      direction="column"
+      className={`gap-10 ${showModal ? 'overflow-hidden' : null}`}
+    >
       <FeedHeader />
-      <PostsList />
+      <PostsList
+        setSelectedComments={setSelectedComments}
+        setShowModal={setShowModal}
+        setSelectedPost={setSelectedPost}
+      />
+      <PostModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        post={selectedPost}
+        comments={selectedComments}
+      />
     </FlexBox>
   );
 }
