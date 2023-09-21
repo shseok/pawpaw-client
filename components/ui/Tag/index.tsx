001e4ce1515aa829756bec41602ae384c9969a08
taejinii <xowls177@gmail.com>
2023-09-21 16:47:40 +0900

    Feat: 태그 제거기능 #55

--- components/ui/Tag/index.tsx
+++ components/ui/Tag/index.tsx
@@ -3,7 +3,7 @@ interface TagType {
 }
 export default function Tag({ tagName }: TagType) {
   return (
-    <li className="rounded-[10px] bg-[#F7F8F9] text-[#74787D] min-w-fit py-1 px-2 ">
+    <li className="rounded-[10px] decoration-none bg-[#F7F8F9] text-[#74787D] min-w-fit py-1 px-2 ">
       #{tagName}
     </li>
   );
