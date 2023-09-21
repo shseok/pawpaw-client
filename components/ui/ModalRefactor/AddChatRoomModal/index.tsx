3a9edbbc39836e51fcb0d207bf3a260e63e02d88
taejinii <xowls177@gmail.com>
2023-09-21 18:09:19 +0900

    Feat: 모달 닫기 버튼 추가 #55

--- components/ui/ModalRefactor/AddChatRoomModal/index.tsx
+++ components/ui/ModalRefactor/AddChatRoomModal/index.tsx
@@ -6,6 +6,8 @@
 import { useState } from 'react';
 import { ModalProps } from '@/types/types';
 import useInput from '@/hooks/common/useInput';
+import XIcon from '@/public/X.svg';
+import ArrowLeftIcon from '@/public/arrow-left.svg';
 import Modal from '../../Modal';
 import TitleInput from './TitleInput';
 import DescriptionInput from './DescriptionInput';
@@ -18,8 +20,19 @@ export default function AddChatRoomModal({ open, onClose }: ModalProps) {
   const [tagList, setTagList] = useState<string[]>([]);
   return (
     <Modal open={open} onClose={onClose}>
-      <div className="flex flex-col w-screen tablet:w-[1028px] h-screen tablet:h-[720px] bg-white p-9 tablet:rounded-[10px]">
-        <div className="flex flex-col gap-6">
+      <div className="flex flex-col w-screen tablet:w-[1028px] h-screen tablet:h-[720px]">
+        <div className="self-end hidden tablet:block">
+          <button type="button" onClick={onClose}>
+            <XIcon className="w-8 h-8" />
+          </button>
+        </div>
+        <div className="flex flex-col h-full gap-6 bg-white p-9 tablet:rounded-[10px]">
+          <header className="flex items-center tablet:hidden">
+            <button type="button" onClick={onClose}>
+              <ArrowLeftIcon className="w-6 h-6" />
+            </button>
+            <span className="flex-1 text-center body1">공개 채팅방 만들기</span>
+          </header>
           <TitleInput title={title} onChangeTitle={onChangeTitle} />
           <div className="flex flex-col gap-3 ">
             <DescriptionInput
