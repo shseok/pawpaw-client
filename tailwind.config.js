25c3567a8bf4199075eb16f31d9cda3bc8ea7efc
taejinii <xowls177@gmail.com>
2023-09-25 20:42:18 +0900

    Feat: react-hook-form 적용하여 리팩토링 및 채팅방생성API 연결 #55

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,6 @@ module.exports = {
           '0%': { opacity: '0', transform: 'scale(0.9)' },
           '100%': { opacity: '1', transform: 'scale(1)' },
         },
-        fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        },
-        fadeOut: {
-          '0%': { opacity: '1' },
-          '100%': { opacity: '0' },
-        },
         scaleUp: {
           '0%': { transform: 'scale(0)' },
           '100%': { transform: 'scale(1)' },
