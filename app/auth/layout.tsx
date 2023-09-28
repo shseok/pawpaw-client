4685cd23cec0d7c2506fa6ab740082575274741b
shseok <shseok0674@gmail.com>
2023-09-28 19:39:13 +0900

    Fix: 위치설정 modal error 해결

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -13,9 +13,10 @@ export default function AuthLayout({
           <Logo className="w-36 h-10" />
         </Link>
       </header>
-      <div className="max-w-[530px] min-h-[504px] bg-white px-[65px] py-[60px] flex flex-col items-center justify-between mx-auto mt-8 gap-[98px] rounded-[10px] shadow-md">
+      <div className="max-w-[530px] max-h-[80%] h-[100%] bg-white px-[60px] py-[60px] flex flex-col items-center justify-between mx-auto mt-8 rounded-[10px] shadow-md">
         {children}
       </div>
+      <div id="modal-root" />
     </div>
   );
 }
