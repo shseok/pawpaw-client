c17f5411709523a3e95af2a930bd35d6396abdc1
shseok <shseok0674@gmail.com>
2023-10-04 23:39:43 +0900

    Fix: 계정 생성시 이메일 유효성 체크 추가

--- components/pages/auth/Register/AuthForm.tsx
+++ components/pages/auth/Register/AuthForm.tsx
@@ -1,6 +1,7 @@
 import { ValidateInput } from '@/components/ui/Input/ValidateInput';
+import { isDuplicatedEmail } from '@/service/auth';
 import { validate } from '@/utils/validate';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 interface Inputs {
@@ -16,9 +17,8 @@ interface Props {
 export default function AuthForm({ setIsActive }: Props) {
   const {
     register,
-    handleSubmit,
     getValues,
-    formState: { errors, isSubmitting, isValid },
+    formState: { errors, isValid },
   } = useForm<Inputs>({
     mode: 'onChange',
   });
@@ -35,32 +35,49 @@ export default function AuthForm({ setIsActive }: Props) {
     setConfirmPasswordValidated(true);
     return true;
   };
+
+  const checkDuplicateEmail = useCallback(async (email: string) => {
+    try {
+      const response = await isDuplicatedEmail(email);
+      return !response.duplicate; // 중복되지 않은 경우 true 반환
+    } catch (error) {
+      console.error('Error checking duplicate email:', error);
+      return false; // 오류 발생 시 false 반환
+    }
+  }, []);
+
   useEffect(() => {
-    isValid ? setIsActive(true) : setIsActive(false);
+    setIsActive(isValid);
   }, [isValid]);
   return (
     <form className="w-full">
-      <p className="text-grey-400 text-end">
+      <p className="text-grey-400 text-end body4">
         <span className="text-red">*</span>는 필수 입력 정보입니다.
       </p>
       <div className="flex flex-col gap-5">
         <div className="flex flex-col gap-2">
-          <label htmlFor="id">
+          {/* eslint-disable jsx-a11y/label-has-associated-control */}
+          <label htmlFor="id" className="body1 text-grey-800">
             아이디<span className="text-red">*</span>
           </label>
           <div className="flex flex-col gap-1">
             <ValidateInput
               type="text"
               id="id"
-              success={emailValidated}
+              success={emailValidated && !errors.email}
               error={!!errors.email}
               placeholder="pawpaw1234@google.com"
               {...register('email', {
                 required: true,
                 pattern: validate.email,
-                validate: () => {
-                  setEmailValidated(true); // 유효성 체크 완료 시 변수 업데이트
-                  return true; // 유효성 검사 결과 반환
+                validate: async (value) => {
+                  if (!value) return true; // 값이 없으면 유효성 검사 통과
+                  setEmailValidated(false); // 유효성 체크 시작 시 변수 초기화
+                  const isUnique = await checkDuplicateEmail(value); // 이메일 중복 확인
+                  setEmailValidated(isUnique); // 중복되지 않은 경우에만 변수 업데이트
+                  return (
+                    isUnique || '동일한 이메일 주소로 가입된 계정이 있습니다. '
+                  );
                 },
               })}
             />
@@ -70,7 +87,8 @@ export default function AuthForm({ setIsActive }: Props) {
           </div>
         </div>
         <div className="flex flex-col gap-2">
-          <label htmlFor="password">
+          {/* eslint-disable jsx-a11y/label-has-associated-control */}
+          <label htmlFor="password" className="body1 text-grey-800">
             비밀번호<span className="text-red">*</span>
           </label>
           <div className="flex flex-col gap-1">
