e8f01c486e538050076c0464ce92921f80c0b98a
taejinii <xowls177@gmail.com>
2023-10-08 23:26:08 +0900

    Update:유저정보 불러오기쿼 refetchOnWindowFocus false 로 설정 #47
    Update:유저정보 불러오기 쿼리 refetchOnWindowFocus false 설정 #47"

--- hooks/queries/useGetUserInfo.ts
+++ hooks/queries/useGetUserInfo.ts
@@ -2,6 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import getUserInfo from '@/service/user';
 
 export default function useGetUserInfo() {
-  const { data } = useQuery(['userInfo'], getUserInfo);
+  const { data } = useQuery({
+    queryKey: ['userInfo'],
+    queryFn: getUserInfo,
+    refetchOnWindowFocus: false,
+  });
   return { data };
 }
