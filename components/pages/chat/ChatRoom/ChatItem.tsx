e9c379a2752980568a2e74c13bb867268e66cf05
taejinii <xowls177@gmail.com>
2023-10-17 12:54:00 +0900

    Fix: 오타수정 #67

--- components/pages/chat/ChatRoom/ChatItem.tsx
+++ components/pages/chat/ChatRoom/ChatItem.tsx
@@ -19,7 +19,7 @@ export default function ChatItem({ ...chat }: ChatType) {
   const chatSelfClass = isWriter ? 'self-end' : 'self-start';
   const chatFlexClass = isWriter ? 'flex-row-reverse' : 'flex-row-rerverse';
   const chatStyleClass = isWriter
-    ? 'bg-primary-100 text-blaek rounded-tr-none'
+    ? 'bg-primary-100 text-black rounded-tr-none'
     : 'bg-primary-200 text-white rounded-tl-none';
 
   if (chatType === 'MESSAGE') {
