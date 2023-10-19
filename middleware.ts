cf4cdc769f628d5ac65eb9b8bd68c70715ea2e4c
shseok <shseok0674@gmail.com>
2023-10-19 16:47:01 +0900

    Fix: Redirect 횟수 초과 버그 해결

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,16 @@ export default function middleware(req: NextRequest) {
   const hasToken = req.cookies.has('REFRESH');
   const pathname = req.nextUrl.pathname;
   const query = pathname === '/' ? '' : `?next=${pathname}`;
-  if (!hasToken) {
-    return NextResponse.redirect(new URL(`/auth/login${query}`, req.nextUrl));
-  } else if (hasToken && pathname.includes('/auth')) {
+  const isAuthPath = req.nextUrl.pathname.startsWith('/auth');
+  if (isAuthPath && hasToken) {
+    // 유저가 토큰을 가지고 auth 페이지에 접근하면 홈으로 리디렉션
     return NextResponse.redirect(new URL('/', req.nextUrl));
   }
+  if (!isAuthPath && !hasToken) {
+    // 유저가 토큰이 없고 로그인 페이지가 아닌 페이지에 권한이 필요한 서비스에 접근하면 로그인 페이지로 리디렉션
+    return NextResponse.redirect(new URL(`/auth/login${query}`, req.nextUrl));
+  }
+  return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
