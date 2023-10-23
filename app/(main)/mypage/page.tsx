e3a3f766a7969bab73186ceb07047dd33f1bec2f
jiyeeeah <sije3757@snu.ac.kr>
2023-10-23 22:30:54 +0900

    Rename: mypage의 page.tsx이름 잘못된 것 수정 #26

--- app/(main)/mypage/page.tsx
+++ app/(main)/mypage/page.tsx
@@ -1,7 +1,7 @@
 import MyPageMain from '@/components/pages/mypage/MyPageMain';
 import SideProfile from '@/components/pages/mypage/SideProfile';
 
-export default function CommunityPage() {
+export default function MyPage() {
   return (
     <main className="flex-1 w-full h-full p-8 overflow-scroll">
       <div className="flex flex-col md:static">
