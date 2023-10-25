a8041afde26000b6ec8c5a87042e3fbdd3e16c2c
taejinii <xowls177@gmail.com>
2023-10-25 16:57:59 +0900

    Update: 채팅룸정보 API credensials 추가

--- app/(main)/chat/[roomId]/page.tsx
+++ app/(main)/chat/[roomId]/page.tsx
@@ -9,6 +9,7 @@ import { cookies } from 'next/headers';
 async function getChatRoomInfo(roomId: string): Promise<ChatRoomInfo> {
   const url = `${process.env.SERVER_API_URL}/api/chatroom/${roomId}`;
   const response = await fetch(url, {
+    credentials: 'include',
     headers: {
       Cookie: `ACCESS=${cookies().get('ACCESS')?.value}`,
     },
