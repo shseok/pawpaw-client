fb5ea2a2516e2f01533565d96486fb72e5abf49e
taejinii <xowls177@gmail.com>
2023-11-03 23:31:45 +0900

    Design: Carousel css 수정 #81

--- app/(main)/community/_components/Carousel.tsx
+++ app/(main)/community/_components/Carousel.tsx
@@ -6,13 +6,13 @@ export default function Carousel({ children }: { children: React.ReactNode }) {
   const [ref] = useKeenSlider({
     breakpoints: {
       '(min-width: 400px)': {
-        slides: { perView: 1, spacing: 5 },
+        slides: { perView: 1, spacing: 20 },
       },
       '(min-width: 768px)': {
-        slides: { perView: 2, spacing: 5 },
+        slides: { perView: 2, spacing: 20 },
       },
       '(min-width: 1000px)': {
-        slides: { perView: 3, spacing: 10 },
+        slides: { perView: 3, spacing: 20 },
       },
     },
   });
