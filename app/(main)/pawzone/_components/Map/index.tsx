283c36edba96e2aaa190e83064fb260ea9b6681c
shseok <shseok0674@gmail.com>
2023-11-28 23:59:10 +0900

    Feat: google-map-react 라이브러리 도입

--- app/(main)/pawzone/_components/Map/index.tsx
+++ app/(main)/pawzone/_components/Map/index.tsx
@@ -1,9 +1,27 @@
 'use client';
 
+import GoogleMapReact from 'google-map-react';
+
+const KEY = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY;
+
 export default function Map() {
+  const cordinates = { lat: 0, lng: 0 };
+  if (!KEY) {
+    throw new Error('Google Map API key is missing');
+  }
+
   return (
     <div className="w-full h-full flex items-center justify-center absolute top-0 left-0">
-      Maps
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: KEY }}
+        defaultCenter={cordinates}
+        center={cordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        // options={}
+        // onChange={}
+        // onChildClick={}
+      ></GoogleMapReact>
     </div>
   );
 }
